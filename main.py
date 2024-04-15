c4c4f432770e741c4dcbb2d8e07bf3b59e75edc7
hsantos2 <159508866+hsantos2@users.noreply.github.com>
2024-04-15 03:54:49 +0000

    Assignment 0-3

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-AllowedVehiclesList = ["Ford F-150","Chevrolet Silverado","Tesla CyberTruck","Toyota Tundra","Nissan Titan"]
+vehiclesList = open("data/vehicles.txt","r")
+AllowedVehiclesList= vehiclesList.read()
 
 # Menu and User input
 
 print("********************************\nAutoCountry Vehicle Finder v0.1\n********************************")
 
-response = input("Please Enter the following number below from the following menu:\n1. PRINT all Authorized Vehicles\n2. SEARCH for Authorized Vehicle\n3. Exit\n")
+response = input("Please Enter the following number below from the following menu:\n1. PRINT all Authorized Vehicles\n2. SEARCH for Authorized Vehicle\n3. ADD Authorized Vehicle\n4. Exit\n")
 
 # Print All Vehicles for loop
 if (response == "1"):
-  for AllowedVehicles in AllowedVehiclesList:
-    print(AllowedVehicles)
+  print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:\n"+ AllowedVehiclesList)
 
 #Search
 if (response == "2"):
@@ -19,9 +19,18 @@ if (response == "2"):
   else:
     print (response + " is not an authorized vehicle, if you received this in error please check the spelling and try again.")
 
+# Append 
+if (response == "3"):
+  vehiclesList = open("data/vehicles.txt","a")
+  response = input("Please enter the full vehicle name you would like to add:\n")
+  vehiclesList.write("\n" + response)
+  print("You have added " + response + " as an authorized vehicle.")
+  
+ 
 
 # Exit if statement
-if (response == "3"):
+if (response == "4"):
   print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+  vehiclesList.close()
 
 exit()
